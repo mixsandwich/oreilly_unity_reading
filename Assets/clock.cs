fbe61e305f0fd49056717dc977b2e08df405c513
octa-gon <mixsandwichdx@gmail.com>
2018-09-29 05:01:31 +0900

    時計のサンプル追加

---
+++ Assets/clock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class clock : MonoBehaviour
+{   
+    float nowTime = 0;
+
+    void FixedUpdate()
+    {
+        nowTime += Time.deltaTime;
+        GetComponent<LineRenderer>().SetPosition(1,new Vector3(Mathf.Sin(2*Mathf.PI*nowTime/60),Mathf.Cos(2*Mathf.PI*nowTime/60),0));      
+    }
+}
